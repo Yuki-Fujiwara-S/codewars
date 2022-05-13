547cd36c38d4a722e3fb3337668c703dd3cb62aa
Sathvik Yuki Fujiwara <sathvikyf@gmail.com>
2022-05-13 15:12:12 -0700

    try different solution

--- multiplies3or5.js
+++ multiplies3or5.js
@@ -5,3 +5,11 @@ function solution(number){
   
   return sum;
 }
+
+
+//Different solution that is interesting
+
+function solution(number){
+  var n3 = Math.floor(--number/3), n5 = Math.floor(number/5), n15 = Math.floor(number/15);
+  return (3 * n3 * (n3 + 1) + 5 * n5 * (n5 + 1) - 15 * n15 * (n15+1)) /2;
+}
