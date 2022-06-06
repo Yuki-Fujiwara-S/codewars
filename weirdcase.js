65fcbcec6a3e34989df4d7226dc5f8b32eb354ae
Sathvik Yuki Fujiwara <sathvikyf@gmail.com>
2022-06-06 16:04:36 -0700

    alternate solution using two maps, remember to use index

--- weirdcase.js
+++ weirdcase.js
@@ -20,3 +20,13 @@ function convertWord(word){
   }
   return newArr.join("");
 }
+
+
+// alternate solution
+function toWeirdCase(string){
+  return string.split(' ').map(function(word){
+    return word.split('').map(function(letter, index){
+      return index % 2 == 0 ? letter.toUpperCase() : letter.toLowerCase()
+    }).join('');
+  }).join(' ');
+}
