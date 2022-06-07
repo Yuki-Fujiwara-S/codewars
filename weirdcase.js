802312e803cf0664992888f5dc8a0b6b1177f788
Sathvik Yuki Fujiwara <sathvikyf@gmail.com>
2022-06-07 15:19:39 -0700

    alternate solution

--- weirdcase.js
+++ weirdcase.js
@@ -30,3 +30,19 @@ function toWeirdCase(string){
     }).join('');
   }).join(' ');
 }
+
+
+// anothersolution
+
+function toWeirdCaseCharacter(chr, index)
+{
+  return index % 2 ? chr.toLowerCase() : chr.toUpperCase();
+}
+
+function toWeirdCaseWord(word){
+  return word.split("").map(toWeirdCaseCharacter).join("");
+}
+
+function toWeirdCase(string){
+  return string.split(" ").map(toWeirdCaseWord).join(" ");
+}
