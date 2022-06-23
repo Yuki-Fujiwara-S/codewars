4462ce47d7168d6c6334a96b3849e5f97fce7184
Sathvik Yuki Fujiwara <sathvikyf@gmail.com>
2022-06-23 10:26:32 -0700

    adding variable for negative boolean

--- maxSequence.js
+++ maxSequence.js
@@ -3,12 +3,31 @@ var maxSequence = function(arr){
     return 0;
   }
   
-  arr.sort();
+  arr.sort(function(a,b){return a - b});
   console.log(arr);
   
   let newArr = [...new Set(arr)].reverse();
   console.log(newArr);
   
-  // return with if statement
+  // need to return 0 if all are negative
+  let negative = true;
+  let sum = 0;
+  
+  // checking if all negative
+  for (num of newArr) {
+    if (num > 0) {
+      negative = false;
+    }
+  }
+  
+  
+  if (negative) {
+    return 0;
+  } 
+  else { 
+    return sum;
+  }
+  
+  
 
 }
