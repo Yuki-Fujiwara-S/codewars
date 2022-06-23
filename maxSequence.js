8b745cc551a634c9b1985b1d0762abe13c598365
Sathvik Yuki Fujiwara <sathvikyf@gmail.com>
2022-06-23 10:12:08 -0700

    tried approach using sort and set

--- maxSequence.js
+++ maxSequence.js
@@ -2,6 +2,13 @@ var maxSequence = function(arr){
   if (arr.length === 0) {
     return 0;
   }
-  /// order it ascending 
-  // use sort a - b built in javascript method
+  
+  arr.sort();
+  console.log(arr);
+  
+  let newArr = [...new Set(arr)].reverse();
+  console.log(newArr);
+  
+  // return with if statement
+
 }
