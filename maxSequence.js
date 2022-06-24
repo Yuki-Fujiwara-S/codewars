dcd4fe5d77193cfed9eac349461b59eca703a773
Sathvik Yuki Fujiwara <sathvikyf@gmail.com>
2022-06-24 12:16:06 -0700

    cad alg

--- maxSequence.js
+++ maxSequence.js
@@ -3,30 +3,7 @@ var maxSequence = function(arr){
     return 0;
   }
   
-  arr.sort(function(a,b){return a - b});
-  console.log(arr);
-  
-  let newArr = [...new Set(arr)].reverse();
-  console.log(newArr);
-  
-  // need to return 0 if all are negative
-  let negative = true;
-  let sum = 0;
-  
-  // checking if all negative
-  for (num of newArr) {
-    if (num > 0) {
-      negative = false;
-      sum += num;
-    }
-  }
-  
-  if (negative) {
-    return 0;
-  } 
-  else { 
-    return sum;
-  }
+  // CadaneS ALGORITHM
   
   
 
