d728ab8601c737fe77dfc30da19a13381adff134
Sathvik Yuki Fujiwara <sathvikyf@gmail.com>
2022-07-08 16:23:37 -0700

    alternate solution using multiple methods at once

--- hashtagGen.js
+++ hashtagGen.js
@@ -29,3 +29,13 @@ function generateHashtag (str) {
 function capitalize(str) {
   return str.charAt(0).toUpperCase() + str.slice(1);
 }
+
+//alternate solution
+function generateHashtag (str) {
+  if(!str || str.length < 1) return false;
+  
+  var r = '#' + str.split(' ').map(function(el) {
+    return el.charAt(0).toUpperCase() + el.slice(1).toLowerCase();
+  }).join('');
+  return r.length > 140?false:r;
+}
