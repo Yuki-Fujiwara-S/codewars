209cc4c863242046169b3c67fbc2a44129d1c7fb
Sathvik Yuki Fujiwara <sathvikyf@gmail.com>
2022-10-06 16:46:58 -0700

    use toString directly

--- bitCounting.js
+++ bitCounting.js
@@ -1,5 +1,6 @@
 var countBits = function(n) {
-  let binary = (n >>> 0).toString(2);
+  let binary = n.toString(2);
+  console.log(binary)
   let count = 0;
   for(let elm of binary){
     if (elm == 1) {
